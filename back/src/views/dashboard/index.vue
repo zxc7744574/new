2b62d42f0ca97702beeca77c9ee96f3c0c60f477
Bug Luv <405449506@qq.com>
2018-11-20 17:12:20 +0800

    vue-element-admin

---
+++ back/src/views/dashboard/index.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name',
+      'roles'
+    ])
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+</style>
